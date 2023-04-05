740affc7c596fb87843c70eaddf572e62c87785f
Nguyen Hoai Phong <andyhoaiphong2003@gmail.com>
2023-04-05 09:40:34 +0700

    add layout

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-const express = require('express')
+import express from 'express';
+import morgan from 'morgan';
+import { engine } from 'express-handlebars';
+import path from "path";
 const app = express()
-const port = 3000
+import { fileURLToPath } from 'url';
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello World<h1>')
+//define filename and dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+//HTTP logger
+app.use(morgan('combined'));
+
+app.use(express.static('public'));
+
+//Template engine
+app.engine('.hbs', engine({ extname: '.hbs' }));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'src/views'));
+
+app.get('/home', (req, res) => {
+    res.render('home')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.get('/feature', (req, res) => {
+    res.render('feature')
 })
+
+app.listen(3000)
